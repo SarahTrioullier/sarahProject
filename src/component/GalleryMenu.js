b5b1599a1ad6f18b0aef6be723eaa1c874184930
Charlene19 <charlene.romain@yahoo.fr>
2020-12-31 12:56:03 +0100

    zoom sur image

--- src/component/GalleryMenu.js
+++ src/component/GalleryMenu.js
@@ -2,7 +2,8 @@ import { SRLWrapper } from "simple-react-lightbox";
 import React from "react";
 import personn from '../images/personn.jpg';
 import travel from '../images/travel.jpg';
-import portrait from '../images/portrait.jpg';
+import travelde from '../images/travel_N9A9788.jpg';
+import portrait from '../images/Portrait.jpg';
 import accueil from '../images/accueil.jpg';
 import stilllife from '../images/stilllife.jpg';
 import Layout from "../Layout";
@@ -16,13 +17,13 @@ const options = {
         marginTop: '300px'
     }
 }
-const imageStyle = {
 
-    height: '100%',
-    width: '100%',
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto'
+const legend = {
+
+fontFamily: 'HelveticaNeueUltraLight',
+fontWeight: 'bold',
+fontSize: 'large',
+
 }
 
 
@@ -31,34 +32,19 @@ const GalleryMenu = () => {
     return (
         <Layout>
             <SRLWrapper  style={options} >
-
-                <div className="container-img">
-                <a href='../images/portrait.jpg'>
-                    <img onContextMenu={(e)=> e.preventDefault()} style={imageStyle} src={portrait}  />
-                </a>
+                <div className="portrait">
+<div className="legendPortrait" style={legend}> Test </div>
+                </div>
+                <div className="stilllife">
+                    <div className="legendPortrait"></div>
+                </div>
+                <div className="travel">
+                    <div className="legendPortrait"></div>
+                </div>
+                <div className="contact">
+                    <div className="legendPortrait"></div>
                 </div>
 
-                <br/>
-                <div onContextMenu={(e)=> e.preventDefault()}  className="container-img">
-                <a href={stilllife}>
-                    <img style={imageStyle} src={stilllife} onContextMenu={false}/>
-                </a>
-                </div>
-                <br/>
-
-                    <div onContextMenu ={(e)=> e.preventDefault()}  className="container-img">
-                <a href={travel} >
-                    <img style={imageStyle} src={travel} onContextMenu={false} />
-                </a>
-                    </div>
-                <br/>
-
-
-                <div  onContextMenu={(e)=> e.preventDefault()} className="container-img">
-                <a href={accueil}>
-                    <img  style={imageStyle} src={accueil} onContextMenu={false}/>
-                </a>
-                </div>
             </SRLWrapper>
         </Layout>
     );
