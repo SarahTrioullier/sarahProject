03a384872918fbb47e5387dad673befdee72b41e
Charlene19 <charlene.romain@yahoo.fr>
2021-01-04 15:33:37 +0100

    hover sur photo avec legend

--- src/component/GalleryMenu.js
+++ src/component/GalleryMenu.js
@@ -4,6 +4,7 @@ import personn from '../images/personn.jpg';
 import travel from '../images/travel.jpg';
 import travelde from '../images/travel_N9A9788.jpg';
 import portrait from '../images/Portrait.jpg';
+import portraitlogo from '../images/Portrait_logo.jpg';
 import accueil from '../images/accueil.jpg';
 import stilllife from '../images/stilllife.jpg';
 import Layout from "../Layout";
@@ -33,14 +34,33 @@ const GalleryMenu = () => {
     return (
         <Layout>
             <SRLWrapper  style={options} >
+
                 <div className="portrait">
-<div className="legendPortrait" >  </div>
+                    <div className="legendPortrait">
+                        Portrait
+                    </div>
                 </div>
-                <div className="portrait_hover">
-                    <div className="legendPortrait" > Test </div>
-                </div>
+
+                {/* Test  overlay */}
+                <div className=" overlay-image _ax "><a href="URL_LIEN">
+                    <img className=" image _ay " src={portrait} alt="Alt text"/>
+                    <div className=" normal _b1 ">
+                        <div className=" text _2 ">
+                        </div>
+                    </div>
+                    <div className=" hover _b0 ">
+                        <img className=" image _ay " src={portraitlogo} alt="Alt text hover"/>
+                        <div className=" text _2 ">Portrait
+                        </div>
+                    </div>
+                </a></div>
+
+                {/* Test  overlay */}
+
+
+
                 <div className="stilllife">
-                    <div className="legendPortrait"></div>
+
                 </div>
                 <div className="travel">
                     <div className="legendPortrait"></div>
